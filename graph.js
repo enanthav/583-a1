1336b02b71404db90eea29fe20a34b00800820e8
Elizabeth Nanthavong <enanthav@gmail.com>
2017-10-04 21:36:54 -0600

    Line graph with incidence

--- graph.js
+++ graph.js
@@ -2,12 +2,18 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+// Set ranges
 var x = d3.scale.linear()
     .range([0, width]);
-
 var y = d3.scale.linear()
     .range([height, 0]);
 
+// Define the line
+var incidenceLine = d3.svg.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y(d.inc_rate); });
+
+// Create SVG canvas
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -16,20 +22,46 @@ var svg = d3.select("body").append("svg")
 
 d3.tsv("incidence_2000-2013.tsv", function(error, data) {
     if (error) throw error;
-
     // Coerce the data to numbers.
     data.forEach(function(d) {
         d.inc_rate = +d.inc_rate;
         d.year = +d.year;
         d.count = +d.count;
     });
+    // Group data by cancer
+    var dataGroup = d3.nest()
+        .key(function(d) {
+            return d.leading_cancer_site;
+        })
+        .entries(data);
+
+    dataGroup.forEach(function(d, i) {
+        console.log(dataGroup[i]);
+    });
 
     // Compute the scales’ domains.
     x.domain([d3.min(data, function(d){ return d.year; }), d3.max(data, function(d){ return d.year; })]);
     y.domain([0, d3.max(data, function(d){
-        console.log(d.inc_rate);
+        //console.log(d.inc_rate);
         return d.inc_rate; })]);
 
+    dataGroup.forEach(function(d, i) {
+        console.log(d.values);
+        svg.append("path")
+            .attr("class", "line")
+            .attr("d", incidenceLine(d.values));
+    });
+
+/*    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);*/
     // Add the x-axis.
     svg.append("g")
         .attr("class", "x axis")
@@ -41,12 +73,12 @@ d3.tsv("incidence_2000-2013.tsv", function(error, data) {
         .attr("class", "y axis")
         .call(d3.svg.axis().scale(y).orient("left"));
 
-    // Add the points!
+/*    // Add the points!
     svg.selectAll(".point")
         .data(data)
         .enter().append("circle")
         .attr("class", "point")
         .attr("r", 4.5)
         .attr("cx", function(d) { return x(d.year); })
-        .attr("cy", function(d) { return y(d.inc_rate); });
+        .attr("cy", function(d) { return y(d.inc_rate); });*/
 });
