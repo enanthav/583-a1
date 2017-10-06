df333fd4b914af9e4bb8c8b84c875460215d6476
Elizabeth Nanthavong <enanthav@gmail.com>
2017-10-05 23:28:13 -0600

    Line graph with incidence and partially functional mortality

--- graph.js
+++ graph.js
@@ -1,6 +1,7 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
+/* Based off tutorial & code from http://www.d3noob.org/2014/07/d3js-multi-line-graph-with-automatic.html */
+var margin = {top: 20, right: 20, bottom: 50, left: 40},
     width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 450 - margin.top - margin.bottom;
 
 // Set ranges
 var x = d3.scale.linear()
@@ -13,14 +14,20 @@ var incidenceLine = d3.svg.line()
     .x(function(d) { return x(d.year); })
     .y(function(d) { return y(d.inc_rate); });
 
+var mortalityLine = d3.svg.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y(d.mortality_rate); });
+
 // Create SVG canvas
-var svg = d3.select("body").append("svg")
+var svg = d3.select("body")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("incidence.tsv", function(error, data) {
+// Get the data
+d3.tsv("incidence_10.tsv", function(error, data) {
     if (error) throw error;
     // Coerce the data to numbers.
     data.forEach(function(d) {
@@ -28,16 +35,13 @@ d3.tsv("incidence.tsv", function(error, data) {
         d.year = +d.Year;
         d.count = +d.Count;
     });
-    // Group data by cancer
+
+    // Group data by cancer type
     var dataGroup = d3.nest()
         .key(function(d) {
             return d["Leading Cancer Sites"];
         })
         .entries(data);
-
-    dataGroup.forEach(function(d, i) {
-        console.log(dataGroup[i]);
-    });
 
 
     // Compute the scales’ domains.
@@ -49,47 +53,45 @@ d3.tsv("incidence.tsv", function(error, data) {
     legendSpace = width/dataGroup.length;
     var color = d3.scale.category20();
 
+
+    // Add the paths
     dataGroup.forEach(function(d, i) {
-        console.log(d.values[i]);
+ /*       console.log(i);
+        console.log(d.values[i]);*/
         svg.append("path")
             .attr("class", "line")
             .style("stroke", function() {
                 return d.color = color(d.key); })
-            .attr("id", 'tag'+d.key.replace(/\s+/g, '')) // assign ID **
+            .attr("id", 'tag'+d.key.replace(/\s+/g, ''))
             .attr("d", incidenceLine(d.values));
 
-
-        // Add the Legend
-        svg.append("text")                                    // *******
-            .attr("x", (legendSpace/2)+i*legendSpace) // spacing // ****
-            .attr("y", height + (margin.bottom/2)+ 5)         // *******
-            .attr("class", "legend")    // style the legend   // *******
-            .style("fill", function() { // dynamic colours    // *******
-                return d.color = color(d.key); })             // *******
-            .on("click", function(){                     // ************
+        // Add the legend
+        svg.append("rect")
+            .attr("x", (legendSpace/2) + i*legendSpace)
+            .attr("y", height + (margin.bottom/2)+ 1)
+            .attr("width", 10)
+            .attr("height", 10)
+            .attr("class", "legend")    // style the legend
+            .style("fill", function() { // dynamic colours
+                return d.color = color(d.key); })
+            .on("click", function(){
                 // Determine if current line is visible
-                var active   = d.active ? false : true,  // ************
-                    newOpacity = active ? 0 : 1;             // ************
+                var active   = d.active ? false : true,
+                    newOpacity = active ? 0 : 1;
                 // Hide or show the elements based on the ID
-                d3.select("#tag"+d.key.replace(/\s+/g, '')) // *********
-                    .transition().duration(100)          // ************
-                    .style("opacity", newOpacity);       // ************
+                d3.select("#tag"+d.key.replace(/\s+/g, ''))
+                    .transition().duration(100)
+                    .style("opacity", newOpacity);
                 // Update whether or not the elements are active
-                d.active = active;                       // ************
-            })                                       // ************
+                d.active = active;
+            });
+
+        svg.append("text")
+            .attr("x", (legendSpace/2) + (i*legendSpace)+ 12)
+            .attr("y", height + (margin.bottom/2)+ 10)
             .text(d.key);
     });
 
-/*    // Add the X Axis
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    // Add the Y Axis
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);*/
     // Add the x-axis.
     svg.append("g")
         .attr("class", "x axis")
@@ -100,13 +102,100 @@ d3.tsv("incidence.tsv", function(error, data) {
     svg.append("g")
         .attr("class", "y axis")
         .call(d3.svg.axis().scale(y).orient("left"));
-
-/*    // Add the points!
-    svg.selectAll(".point")
-        .data(data)
-        .enter().append("circle")
-        .attr("class", "point")
-        .attr("r", 4.5)
-        .attr("cx", function(d) { return x(d.year); })
-        .attr("cy", function(d) { return y(d.inc_rate); });*/
 });
+
+function updateData() {
+    console.log("Update Data");
+    // Get the data again
+    d3.tsv("mortality_10.tsv", function (error, data) {
+        if (error) throw error;
+        // Coerce the data to numbers.
+        data.forEach(function (d) {
+            d.mortality_rate = +d["Age-Adjusted Rate"];
+            d.year = +d.Year;
+            d.count = +d.Count;
+        });
+
+        // Group data by cancer type
+        var cancers = d3.nest()
+            .key(function (d) {
+                return d["Leading Cancer Sites"];
+            })
+            .entries(data);
+
+        // dataGroup.forEach(function(d, i){
+        //     console.log(d.values);
+        // });
+
+        // Compute the scales’ domains.
+        x.domain([d3.min(data, function (d) {
+            return d.year;
+        }), d3.max(data, function (d) {
+            return d.year;
+        })]);
+
+        y.domain([0, d3.max(data, function (d) {
+            //console.log(d.inc_rate);
+            return d.mortality_rate;
+        })]);
+
+        legendSpace = width / cancers.length;
+        var color = d3.scale.category20();
+
+        // Select the section we want to apply our changes to
+        var svg = d3.select("body").transition();
+
+        // Draw the paths
+        cancers.forEach(function (d, i) {
+
+            // console.log(dataGroup.length);
+            console.log(i);
+            // console.log(d.values[i]);
+
+            svg.select(".line")
+                .duration(750)
+                .style("stroke", function () {
+                    return d.color = color(d.key);
+                })
+                .attr("d", mortalityLine(d.values));
+            console.log("RAH");
+
+            // Add the legend
+            svg.select(".rect")
+                .attr("x", (legendSpace / 2) + i * legendSpace)
+                .attr("y", height + (margin.bottom / 2) + 1)
+                .attr("width", 10)
+                .attr("height", 10)
+                .attr("class", "legend")    // style the legend
+                .style("fill", function () { // dynamic colours
+                    return d.color = color(d.key);
+                })
+                .on("click", function () {
+                    // Determine if current line is visible
+                    var active = d.active ? false : true,
+                        newOpacity = active ? 0 : 1;
+                    // Hide or show the elements based on the ID
+                    d3.select("#tag" + d.key.replace(/\s+/g, ''))
+                        .transition().duration(100)
+                        .style("opacity", newOpacity);
+                    // Update whether or not the elements are active
+                    d.active = active;
+                });
+
+
+        });
+
+
+        // Add the x-axis.
+        svg.select(".x.axis")
+            .duration(750)
+            .call(d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.format("d")));
+
+        // Add the y-axis.
+        svg.select(".y.axis")
+            .duration(750)
+            .call(d3.svg.axis().scale(y).orient("left"));
+
+
+    });
+}
